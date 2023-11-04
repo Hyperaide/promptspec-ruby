fbf836d4b0e55cd6692d36ed24dba9888b66e3a2
John Paul <johnarpaul@gmail.com>
2023-11-04 16:23:52 +0000

    changes

--- lib/promptspec.rb
+++ lib/promptspec.rb
@@ -6,7 +6,7 @@ require 'net/http'
 require 'uri'
 
 class PromptSpec
-  attr_reader :file_path, :validate_required_params
+  attr_reader :file_path, :validate_required_params, :parameters, :yaml_content
 
   def initialize(file_path, validate_required_params: true)
     @file_path = file_path
@@ -15,8 +15,9 @@ class PromptSpec
   end
 
   def call(**parameters)
-    validate_required_inputs!(parameters) if @validate_required_params
-    parse_prompt_messages(parameters)
+    @parameters = parameters
+    validate_required_inputs! if @validate_required_params
+    parse_prompt_messages
     construct_endpoint_request
   end
 
@@ -30,21 +31,20 @@ class PromptSpec
     raise ParseError, "YAML parsing error: #{e.message}"
   end
 
-  def validate_required_inputs!(parameters)
+  def validate_required_inputs!
     required_params = @yaml_content.dig('parameters', 'required') || []
-    missing_params = required_params - parameters.keys.map(&:to_s)
+    missing_params = required_params - @parameters.keys.map(&:to_s)
     raise RequiredParameterError, "Missing required parameters: #{missing_params.join(', ')}" unless missing_params.empty?
   end
 
-  def parse_prompt_messages(parameters)
-    messages = @yaml_content['prompt']['messages'].map do |message|
+  def parse_prompt_messages
+    @yaml_content['prompt']['messages'].each do |message|
       content = message['content']
-      parameters.each do |key, value|
-        content.gsub!("{#{key}}", value.to_s)
+      @parameters.each do |key, value|
+        content.gsub!("{#{key}}", value.to_s) if content.include?("{#{key}}")
       end
-      { role: message['role'], content: content }
+      message['content'] = content
     end
-    @yaml_content['prompt']['messages'] = messages
   end
 
   def construct_endpoint_request
@@ -67,7 +67,7 @@ class PromptSpec
   end
 
   def construct_default_endpoint
-    @model = @yaml_content['prompt']['model']
+    @model ||= @yaml_content['prompt']['model']
     case @model
     when 'gpt-4', 'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613'
       'https://api.openai.com/v1/chat/completions'
@@ -77,18 +77,20 @@ class PromptSpec
   end
 
   def construct_headers
-    headers = @yaml_content['headers'] || {}
+    @headers ||= begin
+      headers = @yaml_content.fetch('headers', {})
 
-    if headers.empty?
-      case @model
-      when 'gpt-4', 'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613'
-        api_key = ENV['OPENAI_API_KEY']
-        headers['Authorization'] = "Bearer #{api_key}" if api_key
-      # Add more cases here for other providers
+      if headers.empty?
+        case @model
+        when 'gpt-4', 'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613'
+          api_key = ENV['OPENAI_API_KEY']
+          headers['Authorization'] = "Bearer #{api_key}" if api_key
+        # Add more cases here for other providers
+        end
       end
-    end
 
-    headers['Content-Type'] = 'application/json' unless headers.key?('Content-Type')
-    headers
+      headers['Content-Type'] = 'application/json' unless headers.key?('Content-Type')
+      headers
+    end
   end
 end
